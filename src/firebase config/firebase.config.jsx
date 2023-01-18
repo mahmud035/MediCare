c43d3005efcf9adb429b5d967e3483fb31af2b47
mahmud035 <mahamudulhasan4148@gmail.com>
2023-01-18 20:16:07 +0600

    added firebase config

---
+++ src/firebase config/firebase.config.jsx
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAgGfPu5jNet77LyX0ynhOpP2ELQs1QIwg',
+  authDomain: 'medicare-0.firebaseapp.com',
+  projectId: 'medicare-0',
+  storageBucket: 'medicare-0.appspot.com',
+  messagingSenderId: '103162049823',
+  appId: '1:103162049823:web:e2bf62e007dd8b7d7d6c2a',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
