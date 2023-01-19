d46adafd101a9a79c6e976ec1d940f29ca591b0b
mahmud035 <mahamudulhasan4148@gmail.com>
2023-01-19 10:13:41 +0600

    style add new patient record form

--- src/components/AddRecord/AddRecord.jsx
+++ src/components/AddRecord/AddRecord.jsx
@@ -18,114 +18,121 @@ const AddRecord = () => {
   };
 
   return (
-    <div className="container">
-      <Form onSubmit={handleSubmit(handleAddTasks)}>
-        <Row className="mb-3 row-cols-1 row-cols-md-2">
-          <Col>
-            <Form.Label>Patient Name</Form.Label>
-            <Form.Control
-              {...register('age', {
-                required: 'Patient Name is required',
-              })}
-              type="text"
-              placeholder="John Smith"
-            />
+    <div className="add-new-record-page">
+      <h2 className="text-center py-4">Add A New Patient Record</h2>
+      <div className="container new-record-form-container">
+        <Form onSubmit={handleSubmit(handleAddTasks)}>
+          <Row className="mb-3 row-cols-1 row-cols-md-2">
+            <Col className="mb-3 mb-md-0">
+              <Form.Label className="fw-semibold">Patient Name</Form.Label>
+              <Form.Control
+                {...register('age', {
+                  required: 'Patient Name is required',
+                })}
+                type="text"
+                placeholder="John Smith"
+              />
 
-            {errors.patientName && <p>{errors?.patientName?.message}</p>}
-          </Col>
+              {errors.patientName && <p>{errors?.patientName?.message}</p>}
+            </Col>
 
-          <Col>
-            <Form.Label>Age</Form.Label>
-            <Form.Control
-              {...register('age', {
-                required: ' Age is required',
-              })}
-              type="number"
-              placeholder="47"
-            />
+            <Col>
+              <Form.Label className="fw-semibold">Age</Form.Label>
+              <Form.Control
+                {...register('age', {
+                  required: ' Age is required',
+                })}
+                type="number"
+                placeholder="47"
+              />
 
-            {errors.age && <p>{errors?.age?.message}</p>}
-          </Col>
-        </Row>
+              {errors.age && <p>{errors?.age?.message}</p>}
+            </Col>
+          </Row>
 
-        <Row className="mb-3 row-cols-1 row-cols-md-2">
-          <Col>
-            <Form.Label>Blood Pressure</Form.Label>
-            <Form.Control
-              {...register('bloodPressure', {
-                required: ' Blood Pressure is required',
-              })}
-              type="text"
-              placeholder="120/80"
-            />
+          <Row className="mb-3 row-cols-1 row-cols-md-2">
+            <Col className="mb-3 mb-md-0">
+              <Form.Label className="fw-semibold">Blood Pressure</Form.Label>
+              <Form.Control
+                {...register('bloodPressure', {
+                  required: ' Blood Pressure is required',
+                })}
+                type="text"
+                placeholder="120/80"
+              />
 
-            {errors.bloodPressure && <p>{errors?.bloodPressure?.message}</p>}
-          </Col>
+              {errors.bloodPressure && <p>{errors?.bloodPressure?.message}</p>}
+            </Col>
 
-          <Col>
-            <Form.Label>Phone Number</Form.Label>
-            <Form.Control
-              {...register('phoneNumber', {
-                required: ' Phone number is required',
-              })}
-              type="number"
-              placeholder="+90 000 000 000"
-            />
+            <Col>
+              <Form.Label className="fw-semibold">Phone Number</Form.Label>
+              <Form.Control
+                {...register('phoneNumber', {
+                  required: ' Phone number is required',
+                })}
+                type="number"
+                placeholder="+90 000 000 000"
+              />
 
-            {errors.phoneNumber && <p>{errors?.phoneNumber?.message}</p>}
-          </Col>
-        </Row>
+              {errors.phoneNumber && <p>{errors?.phoneNumber?.message}</p>}
+            </Col>
+          </Row>
 
-        <Row className="mb-3 row-cols-1 row-cols-md-2">
-          <Col>
-            <Form.Label>Disease Name</Form.Label>
-            <Form.Control
-              {...register('diseaseName', {
-                required: ' Disease name is required',
-              })}
-              type="text"
-              placeholder="Flu, Diabetes, Arthritis "
-            />
+          <Row className="mb-3 row-cols-1 row-cols-md-2">
+            <Col className="mb-3 mb-md-0">
+              <Form.Label className="fw-semibold">Disease Name</Form.Label>
+              <Form.Control
+                {...register('diseaseName', {
+                  required: ' Disease name is required',
+                })}
+                type="text"
+                placeholder="Flu, Diabetes, Arthritis "
+              />
 
-            {errors.diseaseName && <p>{errors?.phoneNumber?.message}</p>}
-          </Col>
+              {errors.diseaseName && <p>{errors?.phoneNumber?.message}</p>}
+            </Col>
 
-          <Col>
-            <Form.Label>Given Treatment</Form.Label>
-            <Form.Control
-              {...register('treatment', {
-                required: ' Treatment description is required',
-              })}
-              as="textarea"
-              rows={3}
-              placeholder="Short description about diseases..."
-            />
+            <Col>
+              <Form.Label className="fw-semibold">Given Treatment</Form.Label>
+              <Form.Control
+                {...register('treatment', {
+                  required: ' Treatment description is required',
+                })}
+                as="textarea"
+                rows={3}
+                placeholder="Short description about diseases..."
+              />
 
-            {errors.treatment && <p>{errors?.treatment?.message}</p>}
-          </Col>
-        </Row>
+              {errors.treatment && <p>{errors?.treatment?.message}</p>}
+            </Col>
+          </Row>
 
-        <Row className="mb-3 row-cols-1 row-cols-md-2">
-          <Col>
-            <Form.Label>Next Appointment</Form.Label>
-            <Form.Control
-              {...register('nextAppointment', {
-                required: ' Treatment description is required',
-              })}
-              type="date"
-              placeholder="Patient's disease name"
-            />
+          <Row className="mb-3 row-cols-1 row-cols-md-2">
+            <Col>
+              <Form.Label className="fw-semibold">Next Appointment</Form.Label>
+              <Form.Control
+                {...register('nextAppointment', {
+                  required: ' Treatment description is required',
+                })}
+                type="date"
+                placeholder="Patient's disease name"
+              />
 
-            {errors.nextAppointment && (
-              <p>{errors?.nextAppointment?.message}</p>
-            )}
-          </Col>
-        </Row>
+              {errors.nextAppointment && (
+                <p>{errors?.nextAppointment?.message}</p>
+              )}
+            </Col>
+          </Row>
 
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+          <Button
+            className="fw-semibold btn-register btn-save"
+            variant="primary"
+            type="submit"
+          >
+            Save
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 };
